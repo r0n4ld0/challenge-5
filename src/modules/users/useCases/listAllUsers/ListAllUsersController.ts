6ed6bc2a97804c3c152cc223b2bb8923e6b0180f
Ronaldo Scheffer <r0n4ld0@gmail.com>
2021-07-04 18:04:47 -0300

    first commit

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const user_id = request.header("user_id");
+
+      const allUsers = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(allUsers);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
